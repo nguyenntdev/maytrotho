1d3c8a988143b835853f413b47ae2e3d3c38a80b
Nguyễn N. Thiện <kakangocthien109@gmail.com>
2022-01-04 01:48:17 -0800

    ,

--- src/js/script.js
+++ src/js/script.js
@@ -114,25 +114,6 @@ document.querySelectorAll('.dropdown-toggle').forEach(item => {
     }
 
     /**
-     * Mobile nav toggle
-     */
-    on('click', '.mobile-nav-toggle', function(e) {
-        select('#navbar').classList.toggle('navbar-mobile')
-        this.classList.toggle('bi-list')
-        this.classList.toggle('bi-x')
-    })
-
-    /**
-     * Mobile nav dropdowns activate
-     */
-    on('click', '.navbar .dropdown > a', function(e) {
-        if (select('#navbar').classList.contains('navbar-mobile')) {
-            e.preventDefault()
-            this.nextElementSibling.classList.toggle('dropdown-active')
-        }
-    }, true)
-
-    /**
      * Scrool with ofset on links with a class name .scrollto
      */
     on('click', '.scrollto', function(e) {
